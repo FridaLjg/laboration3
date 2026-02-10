e162da76437258823d21461876ba9dd30cd3937d
Frida Ljungberg <frlj2501@student.miun.se>
2026-02-10 16:56:59 +0100

    Lagt till styling för mobil

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//Funktion för klocka
 function clock() {
     const timeNow = new Date();
     document.getElementById("clock").textContent = timeNow.toLocaleTimeString();
@@ -7,3 +8,13 @@ function clock() {
 
 setInterval(clock, 1000);
 clock();
+
+//Funktion för hamburgermeny
+
+const hamburger = document.querySelector("#menu-toggle");
+const menu = document.querySelector("nav ul");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
+
