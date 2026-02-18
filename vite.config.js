aca773b3cb685108b4251f4eb2f8987c258c02f3
Frida Ljungberg <frlj2501@student.miun.se>
2026-02-18 19:17:13 +0100

    Fixat fel i vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                scss: resolve(__dirname, "sss.html"),
+                scss: resolve(__dirname, "scss.html"),
                 animering: resolve(__dirname, "animering.html")
             }
         }
