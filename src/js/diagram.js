5fa2d7947a4034e20e55b50d9c9a20d3dc48a3ad
Frida Ljungberg <frlj2501@student.miun.se>
2026-02-25 15:48:12 +0100

    Lagt till funktion "createChartStaple"

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -1,5 +1,9 @@
 "use strict";
-let allData = [];
+import { Chart } from 'chart.js/auto';
+
+document.addEventListener("DOMContentLoaded", async () => {
+    loadData()
+});
 
 async function loadData() {
     const url = "https://mallarmiun.github.io/Frontend-baserad-webbutveckling/Moment%205%20-%20Dynamiska%20webbplatser/statistik_sokande_ht25.json";
@@ -9,12 +13,33 @@ async function loadData() {
         const response = await fetch(url);
         const data = await response.json();
 
-        //Lagra globalt
-        allData = data;
-
-        displayData(data);
+        createChartStaple(data);
 
     } catch (error) {
         console.error("Fel " + error);
     }
+}
+
+function createChartStaple(data) {
+    const courses = data.filter(data => data.type === "Kurs")
+    const sorted = courses.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
+    const mostSix = sorted.slice(0, 6);
+
+    const labels = mostSix.map(data => data.name);
+    const values = mostSix.map(data => data.applicantsTotal);
+
+    const ctx = document.getElementById('staple');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Antal sökande',
+                data: values,
+                borderWidth: 1,
+                backgroundColor: 'brown'
+            }]
+        },
+    })
 }
