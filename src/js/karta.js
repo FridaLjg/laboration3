8049c59767d234ab442424f2a9c81d4e364fa45b
Frida Ljungberg <frlj2501@student.miun.se>
2026-02-27 14:25:01 +0100

    Lagt till sökfält

--- src/js/karta.js
+++ src/js/karta.js
@@ -1,11 +1,11 @@
 "use strict";
 
+
 function initMap() {
     const position = { lat: 57.875089, lng: 11.963767 };
 
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 12,
+        zoom: 10,
         center: position,
     });
-
 }
