35ac081d2431bd09ce4f711b161ef76278920453
Frida Ljungberg <frlj2501@student.miun.se>
2026-03-02 15:00:05 +0100

    Lagt till funktionen searchPlace + sökknapp

--- src/js/karta.js
+++ src/js/karta.js
@@ -1,11 +1,45 @@
 "use strict";
-
+let map;
 
 function initMap() {
-    const position = { lat: 57.875089, lng: 11.963767 };
+   map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 57.875089, lng: 11.963767 },
+        zoom: 10
+    });
 
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
-        center: position,
+    //Sökfält
+    const searchInput = document.getElementById("search");
+    const searchButton = document.getElementById("search-button");
+
+    searchButton.addEventListener("click", () => {
+        const quest = searchInput.value;
+        searchPlace(quest);
     });
 }
+
+    async function searchPlace(quest) {
+        const url = "https://places.googleapis.com/v1/places:searchText";
+
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Goog-Api-Key": "AIzaSyD1sJROu-pvac4_UNZTBRMhOopqqpabAzQ",
+                    "X-Goog-FieldMask": "places.displayName,places.location",
+                },
+                body: JSON.stringify({
+                    textQuery: quest,
+                }),
+            });
+
+            const data = await response.json();
+
+            Result(data);
+
+        } catch (error) {
+            console.error("Fel: ", error);
+        }
+    }
+
+    window.initMap = initMap;
