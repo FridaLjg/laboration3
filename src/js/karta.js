5a956c4580cc2f5e553d292b78b9751d371f87a0
Frida Ljungberg <frlj2501@student.miun.se>
2026-03-02 20:51:10 +0100

    Anropar API från geocoding

--- src/js/karta.js
+++ src/js/karta.js
@@ -1,1 +1,18 @@
 "use strict";
+
+async function loadData(city) {
+    const url = new URL("https://geocoding-api.open-meteo.com/v1/search");
+
+    url.searchParams.set('name', city);
+    url.searchParams.set("count", "1");
+
+    //Anropa och läs ut data
+    try {
+        const response = await fetch(url.toString());
+        const data = await response.json();
+        console.log(data)
+
+    } catch (error) {
+        console.error("Fel " + error);
+    }
+}
