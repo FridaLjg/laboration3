5a432a40425f102b79fe98400b6a8a3081910eca
Frida Ljungberg <frlj2501@student.miun.se>
2026-03-02 15:19:43 +0100

    Lagt till markör

--- src/js/karta.js
+++ src/js/karta.js
@@ -2,7 +2,7 @@
 let map;
 
 function initMap() {
-   map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 57.875089, lng: 11.963767 },
         zoom: 10
     });
@@ -17,41 +17,46 @@ function initMap() {
     });
 }
 
-    async function searchPlace(quest) {
-        const url = "https://places.googleapis.com/v1/places:searchText";
+async function searchPlace(quest) {
+    const url = "https://places.googleapis.com/v1/places:searchText";
 
-        try {
-            const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-Goog-Api-Key": "AIzaSyD1sJROu-pvac4_UNZTBRMhOopqqpabAzQ",
-                    "X-Goog-FieldMask": "places.displayName,places.location",
-                },
-                body: JSON.stringify({
-                    textQuery: quest,
-                }),
-            });
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Goog-Api-Key": "AIzaSyD1sJROu-pvac4_UNZTBRMhOopqqpabAzQ",
+                "X-Goog-FieldMask": "places.displayName,places.location",
+            },
+            body: JSON.stringify({
+                textQuery: quest,
+            }),
+        });
 
-            const data = await response.json();
+        const data = await response.json();
+        Result(data);
 
-            Result(data);
-
-        } catch (error) {
-            console.error("Fel: ", error);
-        }
+    } catch (error) {
+        console.error("Fel: ", error);
     }
+}
 
-     function Result(data) {
-        const place = data.places[0];
+function Result(data) {
+    const place = data.places[0];
 
-        const pos = {
-            lat: place.location.latitude,
-            lng: place.location.longitude,
-        };
+    const pos = {
+        lat: place.location.latitude,
+        lng: place.location.longitude,
+    };
 
-        map.setCenter(pos);
-        map.setZoom(13);
-    }
-    
-    window.initMap = initMap;
+    map.setCenter(pos);
+    map.setZoom(13);
+
+    //Markör
+    marker = new google.maps.Marker({
+        position: pos,
+        map: map
+    });
+}
+
+window.initMap = initMap;
